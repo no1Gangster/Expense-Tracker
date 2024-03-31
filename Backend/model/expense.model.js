cb2f7a5addbe5f35be20cae4b7d60c23b2358c29
adyasha <adyashap800@gmail.com>
2024-03-31 08:54:49 +0530

    Updated createdAt to IST @expense.model

--- Backend/model/expense.model.js
+++ Backend/model/expense.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const moment = require("moment-timezone")
 
 const expenseSchema = new mongoose.Schema({
     userId:{
@@ -19,9 +20,16 @@ const expenseSchema = new mongoose.Schema({
         enum:['debit','credit','pending'],
         required:true
     },
-    note:String
-},{
-    timestamps:true //adds createdAt and updatedAt val
+    note:String,
+    createdAt: Date
+})
+
+//middleware --> set timestamp to ist
+expenseSchema.pre("save",function(next){
+    const nowUTC = moment.utc()
+    const nowIST = nowUTC.add(5,"hours").add(30,"minutes").tz("Asia/Kolkata")
+    this.createdAt = nowIST
+    next()
 })
 
 const Expense = mongoose.model("Expense",expenseSchema)
