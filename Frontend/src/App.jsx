e381c77dbd7b653ed46af13e8ac1183f1d20d166
no1Gangster <chiragmcmahon@gmail.com>
2024-04-02 01:24:38 +0530

    Responsive Navbar Added Some Browser Routes created Home page and Stats page created Outlet added to Layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,27 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./layout/layout";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import Stats from "./pages/Stats";
 
 function App() {
 	const router = createBrowserRouter([
 		{
 			path: "/",
 			element: <Layout />,
-			children: [],
+			children: [
+				{path : "", element : <Home />},
+				{path : "/stats", element : <Stats />}
+			],
 		},
 	]);
-	return <></>;
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
