c69f912cfd2cd6ba5f71e77ab2bef61b8b6801e8
adyasha <adyashap800@gmail.com>
2024-04-03 19:50:04 +0530

    update,dlt,sort_by_amt added

--- Backend/controller/expense.controller.js
+++ Backend/controller/expense.controller.js
@@ -31,7 +31,59 @@ async function getExpenseByUserId(req,res){
     }
 }
 
+//Update an expense of a given user
+async function updateExpense(req,res){
+    try{
+        let {userId,expenseId} = req.params
+        let updateData = req.body
+        let expense = await Expense.findOneAndUpdate({userId:userId,_id:expenseId},updateData,{new:true})
+        if(!expense){
+            res.status(404).json({"message":"Expense not found"})
+        }
+        res.status(200).json(expense)
+
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({"message":error.message})
+    }
+}
+
+//Delete an expense of a given user
+async function deleteExpense(req,res){
+    try{
+        let {userId,expenseId}=req.params
+        let expense = await Expense.findOneAndDelete({userId,_id:expenseId})
+        res.status(200).json({expense,"message":"Expense deleted successfully"})
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({"message":error.message})
+    }
+}
+
+//Sort expenses by amount in descending order
+async function getExpenseSortedByAmount(req,res){
+    try{
+        const {userId} = req.params
+        const expenses = await Expense.find({userId}).sort({amount:-1})
+        if(expenses){
+            res.status(200).json(expenses)
+        }
+        else{
+            res.status(404).json({"message":"Expense not found"})
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({"message":error.message})
+    }
+}
+
 module.exports={
     addExpense,
-    getExpenseByUserId
+    getExpenseByUserId,
+    updateExpense,
+    deleteExpense,
+    getExpenseSortedByAmount
 }
--- Backend/route/expense.route.js
+++ Backend/route/expense.route.js
@@ -1,11 +1,17 @@
 const express = require("express")
 const{
     addExpense,
-    getExpenseByUserId
+    getExpenseByUserId,
+    updateExpense,
+    deleteExpense,getExpenseSortedByAmount
 }=require("../controller/expense.controller")
 
 const expenseRouter = express.Router()
 
 expenseRouter.post("/",addExpense)
 expenseRouter.get("/:userId",getExpenseByUserId)
+expenseRouter.put("/:userId/:expenseId",updateExpense)
+expenseRouter.delete("/:userId/:expenseId",deleteExpense)
+expenseRouter.get("/:userId/sorted",getExpenseSortedByAmount)
+
 module.exports = expenseRouter
