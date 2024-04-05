559778188cf853c1656de58e503671611479abbc
adyasha <adyashap800@gmail.com>
2024-04-05 18:22:24 +0530

    User controller:delete user info

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -13,6 +13,25 @@ async function addUser(req,res){
     }
 }
 
+//Delete a user account
+async function deleteUserInfo(req,res){
+    try{
+        const {userId} = req.params
+        const deletedUser = await User.findOneAndDelete({_id:userId})
+        if(deletedUser){
+            res.status(200).json({deletedUser,"message":"User deleted successfully"})
+        }
+        else{
+            res.status(400).json({"message":"User not found"})
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({"message":error.message})
+    }
+}
+
 module.exports={
-    addUser
+    addUser,
+    deleteUserInfo
 }
--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const{
-    addUser
+    addUser,deleteUserInfo
 } = require("../controller/user.controller")
 
 const userRouter = express.Router()
 
 userRouter.post("/",addUser)
+userRouter.delete("/:userId",deleteUserInfo)
 
 module.exports=userRouter
