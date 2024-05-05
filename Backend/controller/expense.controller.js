6f675150783392bd11fbe0f6bfcb6bdf10de6582
unknown <ansumanparija982@gmail.com>
2024-05-05 23:26:15 +0530

    expense controller updated

--- Backend/controller/expense.controller.js
+++ Backend/controller/expense.controller.js
@@ -60,15 +60,9 @@ async function updateExpense(req,res){
         let updateData = req.body
         let expense = await Expense.findOneAndUpdate({userId:userId,_id:expenseId},updateData,{new:true})
         if(!expense){
-<<<<<<< HEAD
             return res.status(404).json({"message":"Expense not found"})
         }
         res.status(200).json({expense,"message":"Expense updated successfully"})
-=======
-            res.status(404).json({"message":"Expense not found"})
-        }
-        res.status(200).json(expense)
->>>>>>> 4a578f0361ef2ecbe7bce6acb6a1d81188a3095c
 
     }
     catch(error){
@@ -176,17 +170,9 @@ async function getExpenseSummaryLastSevenDays(req,res){
 async function getOverview(req,res){
     try {
         const {userId}=req.params
-<<<<<<< HEAD
         const expenses=await Expense.find({userId})
         if(expenses){
             let balance=0,totalDebit=0,totalCredit=0,totalPending=0;
-=======
-        const user=await User.findById(userId)
-        let balance=user.balance
-        const expenses=await Expense.find({userId})
-        if(expenses){
-            let totalDebit=0,totalCredit=0,totalPending=0;
->>>>>>> 4a578f0361ef2ecbe7bce6acb6a1d81188a3095c
             expenses.forEach(expense=>{
                 if(expense.exp_type.toLowerCase()=="credit"){
                     totalCredit+=expense.amount
@@ -199,11 +185,6 @@ async function getOverview(req,res){
                 }
             })
             balance=totalCredit+totalPending-totalDebit;
-<<<<<<< HEAD
-=======
-            user.balance=balance
-            await user.save();
->>>>>>> 4a578f0361ef2ecbe7bce6acb6a1d81188a3095c
             res.status(200).json({totalCredit,totalDebit,totalPending,balance})
         }
         else{
@@ -215,141 +196,6 @@ async function getOverview(req,res){
         res.status(500).json({"message":error.message})
     }
 }
-<<<<<<< HEAD
-=======
-
-async function filterExpenseByDate(req,res){
-    try {
-        let userId=req.params.userId
-        let startDateStr=req.query.startDate
-        let endDateStr=req.query.endDate
-        if(!startDateStr || !endDateStr){
-            res.status(400).json({"message":"Please enter a valid start date and end date"})
-        }
-        const startDate = new Date(startDateStr);
-        const endDate = new Date(endDateStr);
-        if(endDate<=startDate){
-            res.status(400).json({"message":"Please enter a valid To_Date"})
-        }
-        let expenses = await Expense.find({
-            userId,
-            $expr: {
-                $and: [
-                    { $gte: [{ $toDate: "$date" }, startDate] },
-                    { $lte: [{ $toDate: "$date" }, endDate] }
-                ]
-            }
-        });
-        if (expenses.length > 0) {
-            res.status(200).json(expenses);
-        } else {
-            res.status(404).json({ "message": "No transactions found within the specified date range" });
-        }
-
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({"message":error.message})
-    }
-}
-
-async function filterExpenseByMonth(req,res){
-    try {
-        let userId=req.params.userId
-        let year=parseInt(req.query.year)
-        let month=parseInt(req.query.month)
-        if (isNaN(year) || isNaN(month) || month < 1 || month > 12) {
-            res.status(400).json({ "message": "Please provide a valid year and month" });
-        }
-        const startDate = new Date(year, month - 1, 1);
-        const endDate = new Date(year, month, 0, 23, 59, 59);
-        let expenses = await Expense.find({
-            userId,
-            $expr: {
-                $and: [
-                    { $gte: [{ $toDate: "$date" }, startDate] },
-                    { $lte: [{ $toDate: "$date" }, endDate] }
-                ]
-            }
-        });
-    
-        if (expenses.length > 0) {
-            res.status(200).json(expenses);
-        } else {
-            res.status(404).json({ "message": "No transactions found for the specified month and year" });
-        }
-
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({"message":error.message})
-    }
-}
-
-async function filterExpenseByYear(req,res){
-    try {
-        let userId=req.params.userId
-        let year=parseInt(req.query.year)
-        if(isNaN(year)){
-            res.status(400).json({"message":"Please provide a year"})
-        }
-        const startDate = new Date(year, 0, 1); 
-        const endDate = new Date(year + 1, 0, 0, 23, 59, 59);
-        let expenses = await Expense.find({
-            userId,
-            $expr: {
-                $and: [
-                    { $gte: [{ $toDate: "$date" }, startDate] },
-                    { $lte: [{ $toDate: "$date" }, endDate] }
-                ]
-            }
-        });
-    
-        if (expenses.length > 0) {
-            res.status(200).json(expenses);
-        } else {
-            res.status(404).json({ "message": "No transactions found for the specified year" });
-        }
-
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({"message":error.message})
-    }
-}
-async function filterExpenseByType(req,res){
-    try {
-        let userId=req.params.userId
-        let type=req.query.type
-        let expense=await Expense.find({userId,exp_type:type})
-        if(expense.length>0){
-            res.status(200).json(expense)
-        }
-        else{
-            res.status(404).json({"message":`No transactions found for ${type}`})
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({"message":error.message})
-    }
-}
-
-async function filterExpenseByCategory(req,res){
-    try {
-        let userId=req.params.userId
-        let category=req.query.category
-        let categoryRegex=new RegExp(category,'i')
-        let expense=await Expense.find({userId,exp_category:{$regex:categoryRegex}})
-        if(expense.length>0){
-            res.status(200).json(expense)
-        }
-        else{
-            res.status(404).json({"message":`No transactions found for ${category}`})
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({"message":error.message})
-    }
-}
-
->>>>>>> 4a578f0361ef2ecbe7bce6acb6a1d81188a3095c
 
 //Filters
 async function filterExpenseByDate(req,res){
@@ -492,18 +338,9 @@ module.exports={
     getExpenseSummaryLastSevenDays,
     getExpenseSortedByExpenseDate,
     getOverview,
-<<<<<<< HEAD
     filterExpenseByDate,
     filterExpenseByMonth,
     filterExpenseByYear,
     filterExpenseByType,
     filterExpenseByCategory
 }
-=======
-    filterExpenseByCategory,
-    filterExpenseByDate,
-    filterExpenseByMonth,
-    filterExpenseByYear,
-    filterExpenseByType
-}
->>>>>>> 4a578f0361ef2ecbe7bce6acb6a1d81188a3095c
