03a2c58b8d1bc36695b319fdb4c99f8e0e10bdce
unknown <ansumanparija982@gmail.com>
2024-05-07 19:55:18 +0530

    User Model Updated

--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         required:true,
         unique:true
     },
+    password:{
+        type:String,
+        required:true
+    },
     budget:{
         type:Number,
         default:0
